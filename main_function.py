558c937eadc825355d4e5ea906999f1dffc4dde6
Perry661 <dongsiqi661@gmail.com>
2026-01-27 19:25:45 -0800

    Implement task management menu and error handling
    Refactor task management logic to use a menu system and handle JSON decoding errors.

--- main_function.py
+++ main_function.py
@@ -6,32 +6,45 @@ import os
 
 if os.path.exists('data.txt'):
     with open('data.txt', 'r', encoding='utf-8') as f:
-        tasks = json.load(f)
+        try:
+            tasks = json.load(f)
+        except json.JSONDecodeError:
+            tasks = []
 else:
-    tasks = []
+    tasks = []  # NEW OBJECT (list)
 
 
+opts = '0'    # NEW OBJECT (String)
 print('Hello!')
-optInput = input('Wanna input new task? (Y/N)')   # NEW OBJECT (String)
 
-while optInput != 'y' and optInput != 'Y' and optInput != 'n' and optInput != 'N':
-    optInput = input('Please re-enter your option (only Y, N, y, or n): ')
+while True:
+    if opts == '1':
+        AddTask = inputTask.InputTask(tasks)    # NEW OBJECT (class inputTask.InputTask)
+        AddTask.Add()
+        opts = '0'
+        continue
+    elif opts == '2':
+        DeleteTask = delete.removeTask(tasks)   # NEW OBJECT (class delete.removeTask)
+        DeleteTask.Delete()
+        opts = '0'
+        continue
+    elif opts == '3':
+        if not tasks:
+            print('(Nothing is here)')
+        else:
+            for i in tasks:
+                print(i["name"])
+        opts = '0'
+        continue
+    else:
+        # menu starts
+        print('What do you wanna do today?')
+        print('1. Add task(s) \n2. Delete task(s) \n3. View task(s)')   # TODO 第三项先空着，回头加日期排序功能的时候别忘了这儿
+        opts = input('\nEnter your choice here (enter the order number. If end, enter "end"): ')
+        # menu ends
+    if opts == 'end':
+        break
 
-if optInput == 'y' or optInput == 'Y':
-    AddTask = inputTask.InputTask(tasks)
-    AddTask.Add()
-# Above: to input the task
-
-
-optDelete = input('Okay, any task(s) wanna delete? (Y/N)')
-
-while optDelete != 'y' and optDelete != 'Y' and optDelete != 'n' and optDelete != 'N':
-    optDelete = input('Please re-enter your option (only Y, N, y, or n): ') # NEW OBJECT (String)
-
-if optDelete == 'y' or optDelete == 'Y':
-    DeleteTask = delete.removeTask(tasks)
-    DeleteTask.Delete()
-# Above: to delete the task
 
 print('Ok! Remember to do them! ')
 print("Today's task(s): ")
