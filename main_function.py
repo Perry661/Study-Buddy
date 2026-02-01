f11516b755ec611e90dc2602fafeef11245e46c6
Perry661 <dongsiqi661@gmail.com>
2026-01-31 22:20:02 -0800

    Refactor imports and enhance task management options

--- main_function.py
+++ main_function.py
@@ -1,5 +1,6 @@
-import inputTask
-import delete
+from inputTask import InputTask
+from delete import removeTask
+from dueDateFILE import DueDate
 import json
 import os
 
@@ -15,30 +16,61 @@ else:
 
 
 opts = '0'    # NEW OBJECT (String)
-print('Hello!')
+print('\n\nHello!')
 
 while True:
     if opts == '1':
-        AddTask = inputTask.InputTask(tasks)    # NEW OBJECT (class inputTask.InputTask)
+        AddTask = InputTask(tasks)    # NEW OBJECT (class inputTask.InputTask)
         AddTask.Add()
+
         opts = '0'
+        print('\n\n')
+
     elif opts == '2':
-        DeleteTask = delete.removeTask(tasks)   # NEW OBJECT (class delete.removeTask)
+        DeleteTask = removeTask(tasks)   # NEW OBJECT (class delete.removeTask)
         DeleteTask.Delete()
+
         opts = '0'
+        print('\n\n')
+
     elif opts == '3':
+        if not tasks:
+            print('\n(Nothing is here)')
+        else:
+            print('\n')
+            num = 0 # NEW OBJECT (int)
+
+            for i in tasks:
+                print(i["name"])
+                num += 1
+            
+            print(f'\nYou have {num} task(s) left.')
+
+        opts = '0'
+        print('\n\n')
+
+    elif opts == '4':
+        print('\n')
         if not tasks:
             print('(Nothing is here)')
         else:
-            for i in tasks:
-                print(i["name"])
+            d = DueDate()   # NEW OBJECT (class dueDateFILE.DueDate)
+            for j in tasks:
+                j['overDue'] = d.overDue(int(j['dueYear']), int(j['dueMonth']), int(j['dueDay']))
+                print(j["name"] + ', ' + j["overDue"])
+        print('\n')
+
         opts = '0'
+        print('\n\n')
+
     elif opts == 'end':
         break
+
     else:
         # menu starts
         print('What do you wanna do today?')
-        print('1. Add task(s) \n2. Delete task(s) \n3. View task(s)')   # TODO 第三项先空着，回头加日期排序功能的时候别忘了这儿
+        print('1. Add task(s) \n2. Delete task(s) \n3. View task(s) \n4. View due date(s)')
+        # TODO 第三项先空着，回头加日期排序功能的时候别忘了这儿
         opts = input('\nEnter your choice here (enter the order number. If end, enter "end"): ')
         # menu ends
 
