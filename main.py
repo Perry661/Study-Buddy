305f9dcf939c890dbce9b011f8c7524c64f91449
Dong Perry <dongperry@Dongs-MacBook-Air.local>
2026-02-27 15:26:46 -0800

    Refine main function interfaces and simplify save module

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from save import data_path, data_finish_path, save_tasks
+from save import save_tasks
 from add import Add
 from delete import Delete
 from dueDateFILE import DueDate
@@ -6,13 +6,6 @@ from edit import Edit
 from finish import Finish
 import os
 import json
-from datetime import date
-
-
-data_path = 'data.json'
-data_finish_path = 'dataFINISHED.json'
-trash_path = 'trash.json'
-# or enter your saving file
 
 
 def load_tasks(path: str) -> list:
@@ -32,28 +25,44 @@ def handle_add(tasks: list, path: str) -> None:
     print('\n\n')
 
 
+def delete_check(tasks: list, trash: list, trash_path: str) -> None:
+    d = Delete(tasks, trash)  # NEW OBJECT (class delete.Delete)
+    changed = d.deleteChecking()    # NEW OBJECT (Boolean)
+    if changed:
+        save_tasks(trash_path, trash)
+    print('\n\n')
+
+
+def delete_menu() -> str:
+    while True:
+        print('\nDelete Menu:')
+        print('1. Delete \n2. Put back \n3. View having tasks \n4. View deleted tasks')
+        opt = input('Enter your option (if end, enter nothing): ')
+        if opt == '1' or opt == '2' or opt == '3' or opt == '4' or opt == '':
+            return opt
+        else:
+            print('Option out of range, please re-enter.')
+
+
 def handle_delete(tasks: list, path: str, trash: list, trash_path: str) -> None:
     tasks.sort(key=lambda x: x["dueDate"])
-    d = Delete(tasks)  # NEW OBJECT (class delete.Delete)
-    d.deleteTask(trash, trash_path)
+    d = Delete(tasks, trash)  # NEW OBJECT (class delete.Delete)
+    d.deleteTask()
     save_tasks(path, tasks)
     save_tasks(trash_path, trash)
     print('\n\n')
 
 
-def delete_check(tasks: list, trash: list, trash_path: str) -> None:
-    d = Delete(tasks)  # NEW OBJECT (class delete.Delete)
-    changed = d.delete_checking(trash)
-    if changed:
-        save_tasks(trash_path, trash)
+def handle_put_back(tasks: list, path: str, trash: list, trash_path: str):
+    tasks.sort(key=lambda x: x["dueDate"])
+    d = Delete(tasks, trash)  # NEW OBJECT (class delete.Delete)
+    d.putBack()
+    save_tasks(path, tasks)
+    save_tasks(trash_path, trash)
+    print('\n\n')
 
 
-def handle_put_back(tasks, data_path, trash, trash_path):
-    # TODO creat class 
-    pass
-
-
-def handle_edit(tasks: list) -> None:
+def handle_edit(tasks: list, path: str) -> None:
     print('\n')
     tasks.sort(key=lambda x: x["dueDate"])
     e = Edit(tasks)  # NEW OBJECT (class edit.Edit)
@@ -61,7 +70,6 @@ def handle_edit(tasks: list) -> None:
     while True:
         editID, editT = e.editCheck()  # NEW OBJECT (String)
         if editT == '':
-            print('\n\n')
             break
 
         if editT == 'task':
@@ -69,23 +77,21 @@ def handle_edit(tasks: list) -> None:
         else:
             e.editDueDate(editID)
 
-    save_tasks(data_path, tasks)
-
+    save_tasks(path, tasks)
     print('\n\n')
 
 
-def handle_view(tasks: list) -> None:
+def handle_view(tasks: list) -> int:
     print('\n')
     tasks.sort(key=lambda x: x["dueDate"])
+    num = 0  # NEW OBJECT (int)
     if not tasks:
         print('\n(Nothing is here)')
     else:
-        num = 0  # NEW OBJECT (int)
         for i in tasks:
             print(i["name"])
             num += 1
-        print(f'\nYou have {num} task(s) left.')
-    print('\n\n')
+    return num
 
 
 def handle_due_dates(tasks: list, path: str) -> None:
@@ -116,18 +122,18 @@ def handle_finish_task(tasks: list, path: str, taskFINISH: list, pathFINISH: str
                 break
         save_tasks(path, tasks)
         save_tasks(pathFINISH, taskFINISH)
-        print('\n\n')
+    print('\n\n')
 
 
 def prompt_menu() -> str:
     print('What do you wanna do today?')
-    print('1. Add task(s) \n2. Edit task(s) \n3. Delete task(s) \n4. Put back deleted task(s) \n5. View task(s) \n6. View due date(s) \n7. Finish task(s)')
+    print('1. Add task(s) \n2. Edit task(s) \n3. Delete or put back task(s) \n4. View task(s) \n5. View due date(s) \n6. Finish task(s)')
     return input('\nEnter your choice here (enter the order number. If end, enter nothing): ')
 
 
-def show_summary(tasks: list) -> None:
+def show_summary(tasks: list, path: str) -> None:
     tasks.sort(key=lambda x: x["dueDate"])
-    save_tasks(data_path, tasks)
+    save_tasks(path, tasks)
     print('\n')
     print('Remember to do them! ')
     print("Today's task(s): ")
@@ -140,6 +146,11 @@ def show_summary(tasks: list) -> None:
 
 
 def main() -> None:
+    data_path = 'data.json'
+    data_finish_path = 'dataFINISHED.json'
+    trash_path = 'trash.json'
+    # or enter your saving file
+
     tasks = load_tasks(data_path)   # NEW OBJECT (list)
     taskFINISHED = load_tasks(data_finish_path) # NEW OBJECT (list)
     trash = load_tasks(trash_path)  # NEW OBJECT (list)
@@ -154,23 +165,31 @@ def main() -> None:
             handle_add(tasks, data_path)
             opts = '0'
         elif opts == '2':
-            handle_edit(tasks)
+            handle_edit(tasks, data_path)
             opts = '0'
         elif opts == '3':
-            handle_delete(tasks, data_path, trash, trash_path)
+            while True:
+                delete_opt = delete_menu()
+                if delete_opt == '1':
+                    handle_delete(tasks, data_path, trash, trash_path)
+                elif delete_opt == '2':
+                    handle_put_back(tasks, data_path, trash, trash_path)
+                elif delete_opt == '3':
+                    handle_view(tasks)
+                elif delete_opt == '4':
+                    handle_view(trash) 
+                else:
+                    break
             opts = '0'
         elif opts == '4':
-            # TODO reverse deleted task(s)
-            handle_put_back(tasks, data_path, trash, trash_path)
-            print('此功能暂未开放')
+            num = handle_view(tasks)    # NEW OBJECT (int)
+            print(f'\nYou have {num} task(s) left.')
+            print('\n\n')
             opts = '0'
         elif opts == '5':
-            handle_view(tasks)
-            opts = '0'
-        elif opts == '6':
             handle_due_dates(tasks, data_path)
             opts = '0'
-        elif opts == '7':
+        elif opts == '6':
             handle_finish_task(tasks, data_path, taskFINISHED, data_finish_path)
             opts = '0'
         elif opts == '':
@@ -179,7 +198,7 @@ def main() -> None:
             opts = prompt_menu()
 
     save_tasks(data_path, tasks)
-    show_summary(tasks)
+    show_summary(tasks, data_path)
 
 
-main()
+if __name__ == "__main__": main()
